98da5fa70e1ee20908e353e178b9df0bbdca4509
krishna-saurav <krishna.lm99@gmail.com>
2021-05-10 02:22:23 +0530

    modifications in tsconfig json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
+import * as React from "react";
 import logo from './logo.svg';
 import './App.css';
+import Login from './components/login'
 
 const App = ()=> {
   return (
     <div className="App">
       <h1>Welcome to Eduportal app </h1>
+      <Login/>
     </div>
   );
 }
---
+++ src/components/login.tsx
@@ -0,0 +1,12 @@
+import { Button, Card } from "@blueprintjs/core";
+import React from "react";
+
+const Login = ()=> (
+    <Card interactive={true} >
+    <h5><a href="#">Card heading</a></h5>
+    <p>Card content</p>
+    <Button>Submit</Button>
+</Card>
+)
+
+export default Login;
